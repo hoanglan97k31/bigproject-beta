b524844eedaa016b2c7a64e9735071e2d6285f1e
lanlh <lanlh@atmarkcafe.com>
2019-03-06 15:15:00 +0700

    Create Controller and routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,67 @@
+class ProductsController < ApplicationController
+    before_action :find_product, only: [ :show ]
+  def index
+    @products = Product.includes(:category).published
+  end
+
+  def show
+    
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    unless @product.save
+      flash.now[:notice] = fail_message
+      return render :new
+    end
+
+    redirect_to products_path, notice: success_message(:created)
+  end
+
+  def edit
+    render :new    
+  end
+
+  def update
+    unless @product.update(product_params)
+      flash.now[:notice] = fail_message
+      return render :new
+    end
+
+    redirect_to products_path, notice: success_message(:update)
+  end
+
+  def destroy
+    message = fail_message
+    message = success_message(:destroyed) if @product.destroy
+
+    redirect_to products_path, notice: message
+    
+  end
+
+  private
+    def success_message(action)
+      'You have successfully #{action} the product'
+    end
+
+    def fail_message(action)
+      'There is an error in your form'
+    end
+
+    def find_product
+      @product = Product.find params[:id]
+    end
+
+    def product_params
+      params.require(:product).permit(  :tittle,
+                                        :description,
+                                        :price,
+                                        :published,
+                                        :category_id)
+    end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
 
   belongs_to :category
+
+  validates :tittle, :description, :price, presence: true
+
+  scope :published, ->{ where(published: true) }
 end
